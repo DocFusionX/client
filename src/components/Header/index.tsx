d7bee457fbd01f97f7a116d24d80d1d311be635e
Dohyeop Lim <dhlim0817@gmail.com>
2026-02-12 20:43:06 +0100

    feat: add line in header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,8 +2,9 @@ import Logo from "@/assets/logo.svg?react";
 
 export default function Header() {
     return (
-        <div className="w-full flex py-6 px-1 items-center justify-between">
-            <Logo className="h-6 w-auto" />
+        <div className="w-full flex py-6 px-1 items-center justify-between gap-4">
+            <Logo className="h-4 w-auto" />
+            <div className="h-px w-full bg-linear-to-r from-blue-700 to-green-600" />
         </div>
     );
 }

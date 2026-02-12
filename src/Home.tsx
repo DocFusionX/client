e05966f9ed40a610225c27f411c9c12f2d0498e8
Dohyeop Lim <dhlim0817@gmail.com>
2026-02-12 20:40:09 +0100

    feat: reposition chat input

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,10 +1,13 @@
+import ChatContent from "./components/ChatContent";
 import ChatInput from "./components/ChatInput";
 import Header from "./components/Header";
 
 export default function Home() {
     return (
-        <div className="w-full max-w-6xl mx-auto px-6 flex flex-col">
+        <div className="w-full relative max-w-6xl mx-auto px-6 flex flex-col">
             <Header />
+
+            <ChatContent />
 
             <ChatInput />
         </div>
--- src/components/ChatInput/index.tsx
+++ src/components/ChatInput/index.tsx
@@ -4,16 +4,18 @@ import { ArrowUp } from "lucide-react";
 
 export default function ChatInput() {
     return (
-        <div className="flex items-center gap-2">
-            <Field>
-                <Input
-                    id="input-field-chat"
-                    type="text"
-                    placeholder="Enter your message..."
-                />
-            </Field>
-            <div className="p-1.5 rounded-full bg-gray-900 hover:bg-gray-800 cursor-pointer">
-                <ArrowUp className="text-white size-5" />
+        <div className="fixed bottom-0 left-0 right-0 p-4 pb-8 bg-white bg-opacity-90 backdrop-blur-sm border-t border-gray-200">
+            <div className="flex items-center gap-2">
+                <Field>
+                    <Input
+                        id="input-field-chat"
+                        type="text"
+                        placeholder="Enter your message..."
+                    />
+                </Field>
+                <div className="p-1.5 rounded-full bg-gray-900 hover:bg-gray-800 cursor-pointer">
+                    <ArrowUp className="text-white size-5" />
+                </div>
             </div>
         </div>
     );
